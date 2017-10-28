c21f02143220a00e018873941c42df4cc05ed1bb
Shubham Sarwate <sshubham369@gmail.com>
2017-10-28 22:40:03 +0000

    First Major Commit ::wave::

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
 from flask import Flask, render_template, request
 import DGS
-
+import random
+import StringIO
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 app = Flask(__name__)
 
 UPLOAD_FOLDER = os.path.basename('static')
@@ -13,6 +17,7 @@ def hello_world():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    
     file = request.files['image']
     f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
 
@@ -20,14 +25,32 @@ def upload_file():
     file.save(f)
     image='./static/'+file.filename
     print image
-    resolution = 1
-    density = 10
-    dofilter=1
-    maxscale=8
-    notes=8
+    resolution =request.form['resolution']
+    density = request.form['density']
+    dofilter=request.form['dofilter']
+    maxscale=request.form['maxscale']
+    notes=request.form['notes']
     ans =  DGS.dgs(image, density, resolution, dofilter, maxscale, notes,1)
     print ans
     means=ans['mean grain size']
     kuto = ans['grain size kurtosis']
     stdev = ans['grain size skewness']
-    return render_template('output.html',means=means,image=image,stdev=stdev,kuto=kuto)
+    
+    
+    y=ans['grain size frequencies']
+    x=ans['grain size bins']
+    for num in range(len(y)):
+        y[num] *= 100
+    
+
+    
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.bar(x,y,align='center',width=20)
+    fig.savefig('./static/test.png')
+    bar_chart= './static/test.png'
+    
+    return render_template('output.html',means=means,image=image,stdev=stdev,kuto=kuto,bar_chart=bar_chart)
+    
+app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
